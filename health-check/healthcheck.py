a5211f90834a3570584c19a4ec0131a8a913096a
MasterCruelty <robertino.antoniello@gmail.com>
2026-01-03 01:14:41 +0100

    add health check system

---
+++ health-check/healthcheck.py
@@ -0,0 +1,32 @@
+import requests
+import time
+import json
+from datetime import datetime
+
+services = {
+    "user-service": [
+        "http://user-service-1:5000/health",
+        "http://user-service-2:5000/health"
+    ],
+    "orders-service": [
+        "http://orders-service-1:5000/health",
+        "http://orders-service-2:5000/health"
+    ]
+}
+
+while True:
+    status = {}
+
+    for service, urls in services.items():
+        status[service] = {}
+        for url in urls:
+            instance = url.split("//")[1]
+            try:
+                r = requests.get(url, timeout=2)
+                status[service][instance] = "UP"
+            except:
+                status[service][instance] = "DOWN"
+
+    print(json.dumps(status, indent=4), flush=True)
+    print(datetime.now())
+    time.sleep(10)  # ogni 5 secondi ripete il check
